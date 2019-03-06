6a05cfeaddc9554fbe247bd22b15a94e2d0163ae
mrstephenneal <stephen.neal14@gmail.com>
2019-03-06 14:52:23 -0500

    FIX index function return

--- flask-basic5/app.py
+++ flask-basic5/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "How's it going there, we have Flask in a Docker container!"
+    return "How's it going there, we have Flask in a Docker container! Successfully deployed an update!"
 
 
 if __name__ == '__main__':
