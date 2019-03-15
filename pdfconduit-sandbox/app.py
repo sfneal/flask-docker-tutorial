936887e6110f0712c4d69a93da1d45ce23333d39
mrstephenneal <stephen.neal14@gmail.com>
2019-03-15 13:29:30 -0400

    FIX upload file html form to direct to pdfconduit-api

--- pdfconduit-sandbox/app.py
+++ pdfconduit-sandbox/app.py
@@ -29,41 +29,11 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # Check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        params = {
-            'address': '43 Indian Lane',
-            'town': 'Franklin',
-            'state': 'MA',
-        }
-
-        # If user does not select file, browser also submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-
-        # File has been added and validated
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-
-            # Make uploads directory if it does not exist
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.mkdir(app.config['UPLOAD_FOLDER'])
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-
-            # Create new watermarked file and return file path
-            watermarked = apply_watermark(file_path, params)
-            return send_from_directory(app.config['UPLOAD_FOLDER'], os.path.basename(watermarked))
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
+    <form method=post action=http://18.204.43.53/watermark/process enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
